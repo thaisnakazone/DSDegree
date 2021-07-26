e561f3b6e329c99edff4e80cc63789b3287d0144
thaisnakazone <thais.nakazone@alumni.usp.br>
2021-07-25 22:00:28 -0300

    Mini Projeto 2 do Módulo 1 - Data Science Degree
    Continuação do Mini Projeto 1
    
    No arquivo "MiniProjeto2.ipynb", está o programa principal, que chama o "modulo_knn.py", via import, e apresenta a solução do Mini Projeto 1.
    
    No arquivo "modulo_knn.py", está o módulo com a classe e os métodos (funções criadas no Mini Projeto 1).

---
+++ MiniProjeto2/modulo_knn.py
@@ -0,0 +1,109 @@
+class ClassificaPerfil:
+    dicio_perfil_classificado = {}
+    '''
+    Criou-se um dicionário vazio, como atributo estático, que armazenará, a cada iteração do comando "for" (no arquivo MiniProjeto2.ipynb), 
+    o resultado do conjunto de instruções da classe "ClassificaPerfil" (ou seja, esse dicionário armazenará a classificação do perfil dos
+    clientes com carteiras não treinadas).
+    '''
+    
+    
+    def __init__(self, data, no_class, k, ind_no_class):
+        '''
+        A classe "ClassificaPerfil" recebe os seguintes atributos:
+        - uma lista com 120 clientes cujas carteiras já foram treinadas;
+        - uma lista com 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
+        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
+        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados.
+        Além desses atributos, criou-se:
+        - uma lista vazia que armazenará o resultado do conjunto de instruções do método "calcula_dist";
+        - um dicionário vazio que armazenará o resultado do conjunto de instruções do método "freq_perfil_menores_dist".
+        '''
+        self.data = data
+        self.no_class = no_class
+        self.k = k
+        self.ind_no_class = ind_no_class
+        self.lista_dist_ord = []
+        self.dicio_freq_perfil = {}
+        
+    def calcula_dist(self):
+        '''
+        Este método recebe os seguintes argumentos (self), já definidos no método construtor:
+        - a lista com os 120 clientes cujas carteiras já foram treinadas;
+        - a lista com os 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
+        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados.
+        Esse método calcula a distância euclidiana entre dois pontos (uma carteira treinada e outra não treinada).
+        E depois, retorna uma lista com 120 distâncias, ordenadas da mais próxima à mais distante,
+        que foram obtidas comparando-se uma carteira não treinada com as outras 120 já treinadas.
+        '''
+        enum_data = list(enumerate(self.data))
+        enum_no_class = list(enumerate(self.no_class))
+        i = 0
+        lista_dist = []
+        lista_perf_data = []
+        for tupla in enum_data:
+            lista_subtrai = list(zip(enum_no_class[self.ind_no_class][1][2], enum_data[i][1][2]))
+            lista_perf_data.append(enum_data[i][1][1])
+            for indice in range(len(lista_subtrai)):
+                lista_subtrai[indice] = (lista_subtrai[indice][1] - lista_subtrai[indice][0]) ** 2
+            distancia = (sum(lista_subtrai) ** 0.5)
+            lista_dist.append(distancia)
+            lista_dist_perf = list(zip(lista_dist, lista_perf_data))
+            self.lista_dist_ord = sorted(lista_dist_perf)
+            i += 1
+        return self.lista_dist_ord
+
+    def freq_perfil_menores_dist(self):
+        '''
+        Este método recebe os seguintes argumentos (self), já definidos no método construtor:
+        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
+        - o retorno do método anterior (a lista com 120 distâncias).
+        Esse método cria uma lista com o perfil dos 5 (k = 5) clientes (com carteiras treinadas) que obtiveram as distâncias mais próximas,
+        em relação a um cliente com uma carteira não treinada. Depois, verifica-se a frequência de cada perfil nessa lista.
+        Por fim, esse método retorna um dicionário, em que a chave é o perfil do cliente (com carteira treinada) e o valor é a quantidade 
+        de vezes que esse perfil aparece na lista criada anteriormente.
+        '''
+        lista_perfil_menores_dist = []
+        for menores_distancias, perfil in self.lista_dist_ord[:self.k]:
+            if perfil == 'Conservador':
+                perfil_no_class = perfil
+                lista_perfil_menores_dist.append(perfil_no_class)
+            elif perfil == 'Moderado':
+                perfil_no_class = perfil
+                lista_perfil_menores_dist.append(perfil_no_class)
+            elif perfil == 'Agressivo':
+                perfil_no_class = perfil
+                lista_perfil_menores_dist.append(perfil_no_class)
+        
+        for perfil_no_class in lista_perfil_menores_dist:
+            if (perfil_no_class in self.dicio_freq_perfil):
+                self.dicio_freq_perfil[perfil_no_class] += 1
+            else:
+                self.dicio_freq_perfil[perfil_no_class] = 1
+        return self.dicio_freq_perfil
+
+    def classifica_perfil(self):
+        '''
+        Este método recebe os seguintes argumentos (self), já definidos no método construtor:
+        - a lista com os 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
+        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
+        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados;
+        - o retorno do método anterior (dicionário com a frequência do perfil dos 5 clientes com as distâncias mais próximas).
+        Esse método busca (no dicionário do método anterior) o perfil que obteve uma frequência maior do que 50% do valor de k 
+        (ou seja, o perfil que obteve a maior frequência na lista dos 5 clientes com as distâncias mais próximas) e, então, 
+        classifica o cliente com uma carteira não treinada, armazenando essas informações em um dicionário.
+        Depois, retorna-se esse dicionário, em que a chave é o CPF do cliente e o valor é o perfil classificado.
+        '''
+        enum_no_class = list(enumerate(self.no_class))
+        for chave, valor in self.dicio_freq_perfil.items():
+            if valor > self.k*0.5:
+                ClassificaPerfil.dicio_perfil_classificado[enum_no_class[self.ind_no_class][1][0]] = chave
+        return ClassificaPerfil.dicio_perfil_classificado
+            
+    @staticmethod
+    def __repr__():
+        '''
+    Este método não recebe nenhum argumento, pois o dicionário (que deve mostrar os perfis classificados) foi criado 
+    como um atributo estático da classe "ClassificaPerfil".
+    Por fim, retorna-se uma string, contendo a representação do dicionário com os 30 perfis classificados.
+    '''
+        return str(ClassificaPerfil.dicio_perfil_classificado)
