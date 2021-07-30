51c1e66f5db3c269df2c736058a05a4a1a9cc559
thaisnakazone <thais.nakazone@alumni.usp.br>
2021-07-30 18:20:10 -0300

    Alterações nas docstrings

--- MiniProjeto2/modulo_knn.py
+++ MiniProjeto2/modulo_knn.py
@@ -10,13 +10,13 @@ class ClassificaPerfil:
     def __init__(self, data, no_class, k, ind_no_class):
         '''
         A classe "ClassificaPerfil" recebe os seguintes atributos:
-        - uma lista com 120 clientes cujas carteiras já foram treinadas;
-        - uma lista com 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
-        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
-        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados.
+        - data (uma lista com 120 clientes cujas carteiras já foram treinadas);
+        - no_class (uma lista com 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada);
+        - k (valor que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
+        - ind_no_class (índice que identifica cada elemento da lista com os 30 clientes a serem classificados).
         Além desses atributos, criou-se:
-        - uma lista vazia que armazenará o resultado do conjunto de instruções do método "calcula_dist";
-        - um dicionário vazio que armazenará o resultado do conjunto de instruções do método "freq_perfil_menores_dist".
+        - lista_dist_ord (uma lista vazia que armazenará o resultado do conjunto de instruções do método "calcula_dist");
+        - dicio_freq_perfil (um dicionário vazio que armazenará o resultado do conjunto de instruções do método "freq_perfil_menores_dist").
         '''
         self.data = data
         self.no_class = no_class
@@ -28,9 +28,9 @@ class ClassificaPerfil:
     def calcula_dist(self):
         '''
         Este método recebe os seguintes argumentos (self), já definidos no método construtor:
-        - a lista com os 120 clientes cujas carteiras já foram treinadas;
-        - a lista com os 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
-        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados.
+        - self.data;
+        - self.no_class;
+        - self.ind_no_class.
         Esse método calcula a distância euclidiana entre dois pontos (uma carteira treinada e outra não treinada).
         E depois, retorna uma lista com 120 distâncias, ordenadas da mais próxima à mais distante,
         que foram obtidas comparando-se uma carteira não treinada com as outras 120 já treinadas.
@@ -51,8 +51,8 @@ class ClassificaPerfil:
     def freq_perfil_menores_dist(self):
         '''
         Este método recebe os seguintes argumentos (self), já definidos no método construtor:
-        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
-        - o retorno do método anterior (a lista com 120 distâncias).
+        - self.k;
+        - self.lista_dist_ord (o retorno do método anterior, ou seja, a lista com as 120 distâncias).
         Esse método cria uma lista com o perfil dos 5 (k = 5) clientes (com carteiras treinadas) que obtiveram as distâncias mais próximas,
         em relação a um cliente com uma carteira não treinada. Depois, verifica-se a frequência de cada perfil nessa lista.
         Por fim, esse método retorna um dicionário, em que a chave é o perfil do cliente (com carteira treinada) e o valor é a quantidade 
@@ -80,14 +80,15 @@ class ClassificaPerfil:
     def classifica_perfil(self):
         '''
         Este método recebe os seguintes argumentos (self), já definidos no método construtor:
-        - a lista com os 30 clientes a serem classificados, de acordo com sua carteira ainda não treinada;
-        - o valor de k (que define a quantidade de vizinhos a ser utilizada para classificar um perfil);
-        - o índice que identifica cada elemento da lista com os 30 clientes a serem classificados;
-        - o retorno do método anterior (dicionário com a frequência do perfil dos 5 clientes com as distâncias mais próximas).
-        Esse método busca (no dicionário do método anterior) o perfil que obteve uma frequência maior do que a metade do valor de k 
+        - self.no_class;
+        - self.k;
+        - self.ind_no_class;
+        - self.dicio_freq_perfil (o retorno do método anterior, ou seja, o dicionário com a frequência do perfil dos 5 clientes com as
+        distâncias mais próximas).
+        Esse método busca, em "self.dicio_freq_perfil", o perfil que obteve uma frequência maior do que a metade do valor de k 
         (ou seja, o perfil que obteve a maior frequência na lista dos 5 clientes com as distâncias mais próximas) e, então, 
-        classifica o cliente com uma carteira não treinada, armazenando essas informações em um dicionário.
-        Depois, retorna-se esse dicionário, em que a chave é o CPF do cliente e o valor é o perfil classificado.
+        classifica o cliente com uma carteira não treinada, armazenando essas informações em outro dicionário (dicio_perfil_classificado).
+        Depois, retorna-se esse dicionário (dicio_perfil_classificado), em que a chave é o CPF do cliente e o valor é o perfil classificado.
         '''
         for chave, valor in self.dicio_freq_perfil.items():
             if valor > (self.k / 2):
@@ -97,8 +98,8 @@ class ClassificaPerfil:
     @staticmethod
     def mostra_classificacao():
         '''
-        Este método estático não recebe nenhum argumento, pois o dicionário (que deve mostrar os perfis classificados) 
-        foi criado como um atributo estático da classe "ClassificaPerfil".
-        Por fim, retorna-se o dicionário com os 30 perfis classificados.
+        Este método estático não recebe nenhum argumento, pois o dicionário que deve mostrar os perfis classificados
+        (dicio_perfil_classificado) foi criado como um atributo estático da classe "ClassificaPerfil".
+        Por fim, retorna-se esse dicionário com os 30 perfis classificados.
         '''
         return ClassificaPerfil.dicio_perfil_classificado
